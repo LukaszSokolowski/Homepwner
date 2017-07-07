d6b94853c002a63e3e330bead0404e45c95096ab
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-07-07 23:59:48 +0200

    create an image store

---
+++ Homepwner/ImageStore.swift
@@ -0,0 +1,14 @@
+//
+//  ImageStore.swift
+//  Homepwner
+//
+//  Created by Łukasz Sokołowski on 07/07/2017.
+//  Copyright © 2017 Łukasz Sokołowski. All rights reserved.
+//
+
+import UIKit
+
+class ImageStore {
+    
+    let cache = NSCache<NSString,UIImage>()
+}
