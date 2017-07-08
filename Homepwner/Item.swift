af567c2687765378c7ec2c271485f25fcf04a1ca
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-07-08 22:03:53 +0200

    Implemetation of encode method for Item class

--- Homepwner/Item.swift
+++ Homepwner/Item.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Item: NSObject {
+class Item: NSObject, NSCoding {
     
     var name: String
     var valueInDollars: Int
@@ -46,6 +46,14 @@ class Item: NSObject {
                 self.init(name: "", serialNumber: nil, valueInDollars: 0)
         }
    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(dateCreated, forKey: "dateCreated")
+        aCoder.encode(itemKey, forKey: "itemKey")
+        aCoder.encode(serialNumber, forKey: "serialNumber")
+        aCoder.encode(valueInDollars, forKey: "valueInDollars")
+    }
    
 }
 
