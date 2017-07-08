8bf4f0aba9fb0eed692631b626e13955f4185b87
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-07-08 21:41:07 +0200

    Show crosshair in camera mode

--- Homepwner/DetailViewController.swift
+++ Homepwner/DetailViewController.swift
@@ -87,7 +87,23 @@ class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationC
         // If device has a camera take a picture if not pick photo from library
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             imagePicker.sourceType = .camera
-            imagePicker.showsCameraControls = true
+            let overlayView = UIView(frame: imagePicker.cameraOverlayView!.frame)
+            
+            let crosshairLabel = UILabel()
+            crosshairLabel.text = "+"
+            crosshairLabel.font = UIFont.systemFont(ofSize: 100)
+            crosshairLabel.translatesAutoresizingMaskIntoConstraints = false
+            
+            overlayView.addSubview(crosshairLabel)
+            
+            crosshairLabel.centerXAnchor.constraint(equalTo: overlayView.centerXAnchor).isActive = true
+            crosshairLabel.centerYAnchor.constraint(equalTo: overlayView.centerYAnchor).isActive = true
+            
+            // To avoid blocking the underneath default camera controls
+            overlayView.isUserInteractionEnabled = false
+            
+            imagePicker.cameraOverlayView = overlayView
+            
         } else {
             imagePicker.sourceType = .photoLibrary
         }
