50d2759b8385489e216dce449cfbbb7331a2b699
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-07-10 23:00:01 +0200

    Saving items to a file.

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -21,11 +21,13 @@ class ItemStore {
         allItems.append(newItem)
         return newItem
     }
+    
      func removeItem(_ item: Item) {
         if let index = allItems.index(of: item) {
             allItems.remove(at: index)
         }
     }
+    
     func moveItem(from fromIndex: Int, to toIndex: Int) {
         if fromIndex == toIndex {
             return
@@ -38,5 +40,10 @@ class ItemStore {
         
         // Insert item in array at new location
         allItems.insert(movedItem, at: toIndex)
+    }
+    
+    func saveChanges() -> Bool {
+        print("Saving items to: \(itemArchiveURL.path)")
+        return NSKeyedArchiver.archiveRootObject(allItems, toFile: itemArchiveURL.path)
     }
 }
