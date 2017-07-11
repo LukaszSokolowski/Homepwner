8c311f307508d7a8f8132cd3897f02e92a8900f9
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-07-11 22:31:19 +0200

    Creating image URL and transforming it to compressed data.

--- Homepwner/ImageStore.swift
+++ Homepwner/ImageStore.swift
@@ -14,6 +14,15 @@ class ImageStore {
     
     func setImage(_ image: UIImage, forKey key: String) {
         cache.setObject(image, forKey: key as NSString)
+        
+        // Create full URL for image
+        let url = imageURL(forKey: key)
+        
+        // Turn image into JPEG data
+        if let data = UIImageJPEGRepresentation(image, 0.5) {
+        // Write it to full URL
+            let _ = try? data.write(to: url, options: [.atomic])
+        }
     }
     
     func image(forKey key: String) -> UIImage? {
@@ -22,5 +31,11 @@ class ImageStore {
     
     func deleteImage(forKey key: String) {
         cache.removeObject(forKey: key as NSString)
+    }
+    
+    func imageURL(forKey key: String) -> URL {
+        let documentsDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.appendingPathComponent(key)
     }
 }
